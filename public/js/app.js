a32520ccf6809f7d6bc9b448ab5513702ca83bce
Aaron <apurjh@gmail.com>
2017-01-25 16:11:34 +0000

    gets data

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,21 @@
-console.log('hello');
+$(() => {
+  console.log('loaded');
+
+  function getCustomers() {
+    $.ajax({
+      method: 'GET',
+      url: 'http://localhost:8000/customers'
+    })
+    .done((data) => {
+      $.each(data, (index, customer) => {
+        appendCustomer(customer);
+      });
+    });
+  }
+
+  function appendCustomer(customer) {
+    console.log(customer);
+  }
+
+  getCustomers();
+});
