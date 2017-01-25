aa0d7b43da4dc2d433e8441143e53ebb04f311ff
Aaron <apurjh@gmail.com>
2017-01-25 17:35:14 +0000

    99% done but the organisation submit is acting strange

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,38 @@
 $(() => {
-  console.log('loaded');
   $('#newCustomer').on('submit', newCustomer);
+  let type = 'Citizen';
+  const citizenButton = $('#citizenButton');
+  const organisationButton = $('#organisationButton');
+  const anonymousButton = $('#anonymousButton');
+  citizenButton.on('click', citizenForm);
+  organisationButton.on('click', organisationForm);
+  anonymousButton.on('click', anonymousForm);
+
+  function citizenForm() {
+    type = 'Citizen',
+    $('#titleDisplay').show();
+    $('#fNameDisplay').show();
+    $('#lNameDisplay').show();
+
+  }
+
+  function organisationForm() {
+    console.log('what?');
+    type = 'Organisation',
+    $('#titleDisplay').hide();
+    $('#fNameDisplay').hide();
+    $('#lNameDisplay').hide();
+    $('#oNameDisplay').show();
+  }
+
+  function anonymousForm() {
+    type = 'Anonymous',
+    $('#titleDisplay').hide();
+    $('#fNameDisplay').hide();
+    $('#lNameDisplay').hide();
+    $('#oNameDisplay').hide();
+  }
+
 
   function getCustomers() {
     $('#queue').html('');
@@ -16,7 +48,6 @@ $(() => {
   }
 
   function appendCustomer(index, customer) {
-    console.log(customer);
     $('#queue').append(`
       <tr>
         <th scope="row">${index+1}</th>
@@ -29,16 +60,23 @@ $(() => {
   }
 
   function newCustomer(e) {
-    const name = $('#title').val() + ' ' + $('#first_name').val() + ' ' + $('#last_name').val();
+    let name;
+    if (type === 'Citizen'){
+      name = $('#title').val() + ' ' + $('#first_name').val() + ' ' + $('#last_name').val();
+    } else if (type === 'Organisation') {
+      name = $('#org_name').val();
+    } else {
+      name = 'Anonymous';
+    }
     const service = $('input[name=service]:checked').val();
-    console.log(service);
     e.preventDefault();
     $.ajax({
       method: 'POST',
       url: 'http://localhost:8000/customers',
       data: {
         name: name,
-        service: service
+        service: service,
+        type: type
       }
     }).done(() => {
       getCustomers();
